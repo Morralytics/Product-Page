893c88b4640e3cd2b27526a4676b5179d6d55e17
Nick Morris <nicholasmorris2599@gmail.com>
2023-08-15 16:22:10 -0700

    Finished styling the basic header items. Still need to adjust hover effects,

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -22,7 +22,7 @@ const Header = () => {
                     <img src={cartIcon} alt='cart-icon'></img>
                 </li>
                 <li id='profile'>
-                <img src={profilePic} alt='cart-icon' width='32'></img>
+                <img src={profilePic} alt='cart-icon' width='48'></img>
                 </li>
             </ul>
         </nav>
