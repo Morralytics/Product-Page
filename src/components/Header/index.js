40354d3f90329bba5385464b901e10140c361784
Nick Morris <nicholasmorris2599@gmail.com>
2023-08-14 20:12:48 -0700

    Directory setup for header and footer component. Working functional component for header in test.

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="App">
+      <header className="App-header">Test header</header>
+    </div>
+  )
+}
+
+export default Header;
