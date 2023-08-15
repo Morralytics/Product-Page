e8e0820140ec18da9704a354f59764bf7fd98478
Nick Morris <nicholasmorris2599@gmail.com>
2023-08-15 10:23:59 -0700

    Importing images over and adding an assets file to hold these temporary images. Deployment will need to have images in public so I am leaving them there.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React from 'react';
+// Imports that will need to transition to public folder on deployment
+// import cartIcon from '../../../public/images/icon-cart.svg'
+import cartIcon from '../../assets/images/icon-cart.svg';
+import profilePic from '../../assets/images/image-avatar.png';
 
 const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">Test header</header>
-    </div>
+    <header id='header'>
+        <nav id='nav-bar'>
+            <h2 id='brand-title'>sneakers</h2>
+            <ul id='collections-list' class='nav-items'>
+                <li id='collections'>Collections</li>
+                <li id='men'>Men</li>
+                <li id='women'>Women</li>
+                <li id='about'>About</li>
+                <li id='contact'>Contact</li>
+            </ul>
+            <ul id='user-items' class='nav-items'>
+                <li id='cart'>
+                    <img src={cartIcon} alt='cart-icon'></img>
+                </li>
+                <li id='profile'>
+                <img src={profilePic} alt='cart-icon'></img>
+                </li>
+            </ul>
+        </nav>
+    </header>
   )
 }
 
