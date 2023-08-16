c650b990ac4a32970467282f83d4d92ccbd31602
Nick Morris <nicholasmorris2599@gmail.com>
2023-08-16 16:36:59 -0700

    Change of approach after considering how to organize the card information. I am passing the fetched data via props to the support cards so they can render off a single fetch while keeping the main Card componenet as the main image. Considering options to change this main photo based on child hover events however I will need to research more on how to do so.

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import SupportCard from '../SupportCard';
 
 const Card = () => {
 
@@ -13,12 +14,15 @@ const Card = () => {
       .then((data) => setCards(data))
   }, []);
 
-  const card = cards.map((card, i) => {
-    return <li key={i}><img src={card.img} alt={'card' + i} id={'card' + i}/></li>
+  // This is now rendering each card as their own SupportCard child that will be rendered into this parent component
+  // Next I need to render a single card that can change when the child element is hovered (not sure if that is possible)
+  const card = cards.map((card) => {
+    return <SupportCard key={card.i} card={card}/>
   });
 
   return (
     <div>
+      <div>Placeholder</div>
       <ul>
         {card}
       </ul>
