575e4955529e2f80f6ca7641218ecc32d898ca00
Nick Morris <nicholasmorris2599@gmail.com>
2023-08-16 14:19:16 -0700

    Added all the react functional components I will need along with the associated component directories.

--- src/components/CardDetails/index.js
+++ src/components/CardDetails/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Footer = () => {
+const CardDetails = () => {
   return (
     <div>
-      
+
     </div>
   )
 };
 
-export default Footer;
+export default CardDetails;
