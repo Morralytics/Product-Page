110b7c78533bcc05682033dff442573e7426c3c8
Nick Morris <nicholasmorris2599@gmail.com>
2023-08-17 14:48:36 -0700

    Working through trying to get out of an infinite loop when selecting the first image on mount.

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import SupportCard from '../SupportCard';
 
 import './style.css';
@@ -6,6 +6,7 @@ import './style.css';
 const Card = () => {
 
   const [cards, setCards] = useState([]);
+  const [selectedImage, setSelectedImage] = useState([])
 
   // I am using state to save the fetched data into state
   // I am using the useEffect hook here with an empty dependency ( , [] ) so that it will only render once on DOM load
@@ -15,21 +16,31 @@ const Card = () => {
       .then((response) => response.json())
       .then((data) => setCards(data))
   }, []);
-  console.log(cards)
+
+  const [state, dispatch] = useReducer(
+    (state, action) => ({
+      ...state,
+      ...action,
+    }),
+    {
+      mainImage: ''
+    }
+  );
   // This is now rendering each card as their own SupportCard child that will be rendered into this parent component
   // Next I need to render a single card that can change when the child element is hovered (not sure if that is possible)
   const card = cards.map((card, i) => {
     return <SupportCard key={i} keyId={i} card={card}/>
   });
-
+  
   return (
     <div>
-      {cards.length ? (
-        <div><img src={cards && cards[0].img} alt='main-card' id='main-card' width='500px'/></div>
+      {/* {cards.length ? (
+        displayCard()
       ) : (
       <></>
-      )}
+      )} */}
       
+      {/* This new displayCard function will render an image that has been saved to selectedImage state */}
       <ul id='image-collection'>
         {card}
       </ul>
