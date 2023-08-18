216598a98f9317e0ad46d85269f032732403a9b5
Nick Morris <nicholasmorris2599@gmail.com>
2023-08-17 18:15:34 -0700

    Adjusted state to handle state allowing me to set the main card while also being able to change what is displayed by a dispatch function.

--- src/components/SupportCard/index.js
+++ src/components/SupportCard/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import './style.css'
 
-const SupportCard = ({keyId, card}) => {
-  console.log(keyId)
+const SupportCard = ({keyId, card, onClick}) => {
   // Currently rendering the list of photos again
   // Considering having the supporting images be a list and the main card being the main image that changes based on onClick event
   return (
-      <li id='support-card'><img src={card.img} alt={'card' + keyId} id={'card' + keyId} width='100px'/></li>
+      <li id='support-card'><img src={card.img} alt={'card' + keyId} id={'card' + keyId} width='100px' /></li>
   )
 };
 
