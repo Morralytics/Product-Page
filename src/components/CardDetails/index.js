99714a975c236d01fe8f97520ac5fe194dd3656d
Nick Morris <nicholasmorris2599@gmail.com>
2023-08-18 16:01:51 -0700

    Adding information to the card details component.

--- src/components/CardDetails/index.js
+++ src/components/CardDetails/index.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 const CardDetails = () => {
   return (
-    <div>
-      Placeholder
-    </div>
+    <section>
+      <p>SNEAKER COMPANY</p>
+      <h1>Fall Limited Edition Sneakers</h1>
+      <p>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.</p>
+      <aside>
+        <h2>$125.00</h2>
+        <p>50%</p>
+        <p>$250.00</p>
+      </aside>
+    </section>
   )
 };
 
