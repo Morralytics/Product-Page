fd0fb2767e48500674397fbd83d68ef8af4b5d0c
Nick Morris <nicholasmorris2599@gmail.com>
2023-08-18 16:03:43 -0700

    Added id information for each tag.

--- src/components/CardDetails/index.js
+++ src/components/CardDetails/index.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 const CardDetails = () => {
   return (
-    <section>
-      <p>SNEAKER COMPANY</p>
-      <h1>Fall Limited Edition Sneakers</h1>
-      <p>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.</p>
-      <aside>
-        <h2>$125.00</h2>
-        <p>50%</p>
-        <p>$250.00</p>
+    <section id='card-details'>
+      <p id='company-header'>SNEAKER COMPANY</p>
+      <h1 id='card-details-header'>Fall Limited Edition Sneakers</h1>
+      <p id='card-details-description'>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.</p>
+      <aside id='card-price-info'>
+        <h2 id='current-price'>$125.00</h2>
+        <p id='discount-amount'>50%</p>
+        <p id='original-price'>$250.00</p>
       </aside>
     </section>
   )
