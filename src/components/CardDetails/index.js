a996bc881c4fa052b0690339c75f74de44cd9b09
Nick Morris <nicholasmorris2599@gmail.com>
2023-08-19 20:39:38 -0700

    Adjusting the price sections to a table format for easy display of information.

--- src/components/CardDetails/index.js
+++ src/components/CardDetails/index.js
@@ -7,11 +7,15 @@ const CardDetails = () => {
       <p id='company-header'>SNEAKER COMPANY</p>
       <h1 id='card-details-header'>Fall Limited Edition Sneakers</h1>
       <p id='card-details-description'>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.</p>
-      <aside id='card-price-info'>
-        <h2 id='current-price'>$125.00</h2>
-        <p id='discount-amount'>50%</p>
-        <p id='original-price'>$250.00</p>
-      </aside>
+      <table id='card-price-info'>
+        <tr>
+          <td id='current-price'>$125.00</td>
+          <td id='discount-amount'>50%</td>
+        </tr>
+        <tr>
+          <p id='original-price'>$250.00</p>
+        </tr>
+      </table>
     </section>
   )
 };
