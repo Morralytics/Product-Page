b83908c5b9d7fa9ca5ed427baebbd26b378d526e
Nick Morris <nicholasmorris2599@gmail.com>
2023-08-20 14:46:49 -0700

    Working towards utilizing state for counter amount.

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useReducer, useState } from 'react';
 
 const Counter = () => {
+
+  const [counter, setCounter] = useReducer(
+    (state, action) => ({
+      ...state,
+      ...action
+    }),
+    {
+      count: 0,
+    })
+
+  const itemCountPlus = () => {
+    let currentCounter = counter
+    setCounter(currentCounter++)
+  }
+
+  const itemCountMinus = () => {
+
+  }
+
   return (
     <table>
       <tr>
         <td>-</td>
-        <td>0</td>
-        <td>+</td>
+        <td>{counter}</td>
+        <td onClick={itemCountPlus}>+</td>
       </tr>
     </table>
   )
